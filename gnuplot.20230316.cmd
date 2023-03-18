1024a77b3bd002245b97ec7b03ae866ef6020fa2
NasdaqGodzilla <26323326+NasdaqGodzilla@users.noreply.github.com>
2023-03-18 16:49:55 +0800

    gnuplot.cmd: Read data file via ARG1

--- gnuplot.20230316.cmd
+++ gnuplot.20230316.cmd
@@ -1,5 +1,8 @@
+# Usage: gnuplot -c gnuplot.20230316.cmd perfsnippet_20230314_201937.data
+# ARG0: Script file
+# ARG1: Data file
+
 # Init
-set title "PerfSnippet Report" # 添加额外的Description到title
 set xlabel "second(s)"
 set grid
 set key box
@@ -25,7 +28,7 @@ set y2range [0:100]
 # Render
 set size 1.0, 1.0
 set term svg size 1000, 2000 #"Helvetica" 16
-set output 'output_perfsnippet_'.strftime('%Y%m%d@%H%M%S', time(0)).'.svg'
+set output sprintf("output_%s@%s.svg", ARG1, strftime('%Y%m%d_%H%M%S', time(0)))
 set multiplot layout 5,1
 set autoscale x
 set autoscale y
@@ -33,40 +36,40 @@ set autoscale y
 ## Full
 set title "PerfSnippet Report(Full)"
 plot \
-    "perfsnippet_20230314_201937.data" u 1:2 axis x1y1 t 'MemFree(MB)' s b lw 2, \
-    "perfsnippet_20230314_201937.data" u 1:3 axis x1y1 t 'MemAvai(MB)' s b lw 2, \
-    "perfsnippet_20230314_201937.data" u 1:4 axis x1y2 t 'CPU Usage(%)' s b lw 2, \
-    "perfsnippet_20230314_201937.data" u 1:5 axis x1y2 t 'CPU USR(%)' s b lw 2, \
-    "perfsnippet_20230314_201937.data" u 1:6 axis x1y2 t 'CPU SYS(%)' s b lw 2, \
-    "perfsnippet_20230314_201937.data" u 1:7 axis x1y2 t 'CPU Other(%)' s b lw 2, \
-    "perfsnippet_20230314_201937.data" u 1:8 axis x1y2 t 'CPU Idle(%)' s b lw 2, \
+    ARG1 u 1:2 axis x1y1 t 'MemFree(MB)' s b lw 2, \
+    ARG1 u 1:3 axis x1y1 t 'MemAvai(MB)' s b lw 2, \
+    ARG1 u 1:4 axis x1y2 t 'CPU Usage(%)' s b lw 2, \
+    ARG1 u 1:5 axis x1y2 t 'CPU USR(%)' s b lw 2, \
+    ARG1 u 1:6 axis x1y2 t 'CPU SYS(%)' s b lw 2, \
+    ARG1 u 1:7 axis x1y2 t 'CPU Other(%)' s b lw 2, \
+    ARG1 u 1:8 axis x1y2 t 'CPU Idle(%)' s b lw 2, \
 
 ## Mem
 ### CPU Usage + MemFree + MemAvai
 set title "PerfSnippet Report(CPU Usage + Mem)"
 plot \
-    "perfsnippet_20230314_201937.data" u 1:2 axis x1y1 t 'MemFree(MB)' s b lw 2, \
-    "perfsnippet_20230314_201937.data" u 1:3 axis x1y1 t 'MemAvai(MB)' s b lw 2, \
-    "perfsnippet_20230314_201937.data" u 1:4 axis x1y2 t 'CPU Usage(%)' s b lw 2, \
+    ARG1 u 1:2 axis x1y1 t 'MemFree(MB)' s b lw 2, \
+    ARG1 u 1:3 axis x1y1 t 'MemAvai(MB)' s b lw 2, \
+    ARG1 u 1:4 axis x1y2 t 'CPU Usage(%)' s b lw 2, \
 
 ### MemFree
 set title "PerfSnippet Report(MemFree)"
 plot \
-    "perfsnippet_20230314_201937.data" u 1:2 axis x1y1 t 'MemFree(MB)' s b lw 2, \
+    ARG1 u 1:2 axis x1y1 t 'MemFree(MB)' s b lw 2, \
 
 ### MemAvai
 set title "PerfSnippet Report(MemAvai)"
 plot \
-    "perfsnippet_20230314_201937.data" u 1:3 axis x1y1 t 'MemAvai(MB)' s b lw 2, \
+    ARG1 u 1:3 axis x1y1 t 'MemAvai(MB)' s b lw 2, \
 
 ## CPU
 ### CPU Details
 set title "PerfSnippet Report(CPU)"
 plot \
-    "perfsnippet_20230314_201937.data" u 1:4 axis x1y2 t 'CPU Usage(%)' s b lw 2, \
-    "perfsnippet_20230314_201937.data" u 1:5 axis x1y2 t 'CPU USR(%)' s b lw 2, \
-    "perfsnippet_20230314_201937.data" u 1:6 axis x1y2 t 'CPU SYS(%)' s b lw 2, \
-    "perfsnippet_20230314_201937.data" u 1:7 axis x1y2 t 'CPU Other(%)' s b lw 2, \
+    ARG1 u 1:4 axis x1y2 t 'CPU Usage(%)' s b lw 2, \
+    ARG1 u 1:5 axis x1y2 t 'CPU USR(%)' s b lw 2, \
+    ARG1 u 1:6 axis x1y2 t 'CPU SYS(%)' s b lw 2, \
+    ARG1 u 1:7 axis x1y2 t 'CPU Other(%)' s b lw 2, \
 
 ### Split
 # set xrange [0:300]
