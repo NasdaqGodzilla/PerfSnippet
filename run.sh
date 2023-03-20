756e49618a58c4b17c8d6beaa498cf653e050e65
NasdaqGodzilla <26323326+NasdaqGodzilla@users.noreply.github.com>
2023-03-20 16:08:24 +0800

    run.sh: Supports args; Supports gnuplot drawing

--- run.sh
+++ run.sh
@@ -12,9 +12,21 @@ function ondatapulled() {
     echo Generatng for "$datapath"
 
     module_import gnuplot/gnuplot_renderer.sh
-    plot_render_init "$datapath"
-    plot_render_cmd
+    # plot_render_init "$datapath"
+    plot_render_init "$ps_gendatafile"
+    cd "$ps_gendatapath"
     plot_render_plot
+    cd -
+}
+
+ENABLE_MEMINFO=true
+[[ "1" == "$ps_meminfo_disabled" ]] && { \
+    ENABLE_MEMINFO=false
+}
+
+ENABLE_CPUINFO=true
+[[ "1" == "$ps_cpuinfo_disabled" ]] && { \
+    ENABLE_CPUINFO=false
 }
 
 trap onexit SIGHUP SIGINT SIGTERM EXIT
@@ -26,7 +38,7 @@ ps_gendatafile="perfsnippet_$ps_startdatetime.data"
 ps_gendatapath="output/$ps_startdatetime"
 
 module_envsetup
-$ADB_CMD_SHELL "(cd $MODULE_ADBPATH/perfsnippet/; source perfsnippet.sh; ps_startdatetime=$ps_startdatetime perfsnippet)"
+$ADB_CMD_SHELL "(cd $MODULE_ADBPATH/perfsnippet/; source perfsnippet.sh; ps_startdatetime=$ps_startdatetime ps_debug=$ps_debug ps_interval=$ps_interval ps_duration=$ps_duration ps_adbtarget=$ps_adbtarget perfsnippet)"
 module_pulltestdata
 ondatapulled
 trap - SIGHUP SIGINT SIGTERM EXIT
